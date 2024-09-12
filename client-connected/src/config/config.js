1efd4229e0a8dcad1cfff8d67c397fac45aea24e
Aslam Muhammed <azlamazl.aa@gmail.com>
2024-09-12 20:27:11 +0530

    first commit

--- client-connected/src/config/config.js
+++ client-connected/src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     API_URL: "http://localhost:3000/hommlieserver",
     GMAP_KEY: "AIzaSyCwqf4NpjMnz8J-LuEwgJAdVrn_1_5Zt6g&libraries=places",
-    VITE_BASE_URL: "/hommlie",
+    VITE_BASE_URL: "/",
     RAZORPAY_KEY_ID: "rzp_live_ziQnMov0ucEqGL",
     RAZORPAY_KEY_SECRET: "CAqffXj972VdF1bpVVLi577z"
 };
