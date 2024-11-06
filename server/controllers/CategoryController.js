e689e8583468dae134f99b95ce57db34f2c04a2a
apple <apple@apples-MacBook-Pro-2.local>
2024-11-06 13:39:54 +0530

    nov 6

--- server/controllers/CategoryController.js
+++ server/controllers/CategoryController.js
@@ -72,7 +72,7 @@ exports.getSubcategory = async (req, res) => {
         ],
         where: {
           subcat_id: sub.id,
-          // status: 1,
+          status: 1,
         },
         order: [['id', 'DESC']],
       });
