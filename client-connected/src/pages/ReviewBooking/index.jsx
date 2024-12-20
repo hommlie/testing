8b624f54553cf57f110266c8728b2d53d614e479
apple <apple@apples-MacBook-Pro-2.local>
2024-12-20 19:48:58 +0530

    Dec 20

--- client-connected/src/pages/ReviewBooking/index.jsx
+++ client-connected/src/pages/ReviewBooking/index.jsx
@@ -99,7 +99,7 @@ export default function ReviewBooking() {
             if (paymentType?.payment_name === "Online") {
                 try {
                     const orderResponse = await axios.post(`${config.API_URL}/api/initiatePayment`, {
-                        amount: totalAmount,
+                        amount: totalAmount - couponDiscount,
                         currency: "INR",
                         user_id: user.id,
                     }, {
