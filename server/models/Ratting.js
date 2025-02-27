60e9bd7e1533a4633d5332454f862a443c8c4411
mohammed aslam <azlamazl.aa@gmail.com>
2025-02-27 17:08:35 +0530

    feb 27

--- server/models/Ratting.js
+++ server/models/Ratting.js
@@ -1,52 +1,55 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Ratting extends Model {}
 
-Ratting.init({
+Ratting.init(
+  {
     product_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     vendor_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        // references: {
-        //     model: 'users',
-        //     key: 'id'
-        // }
+      type: DataTypes.INTEGER,
+      // allowNull: false,
+      // references: {
+      //     model: 'users',
+      //     key: 'id'
+      // }
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        // references: {
-        //     model: 'users', 
-        //     key: 'id'
-        // }
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // references: {
+      //     model: 'users',
+      //     key: 'id'
+      // }
     },
     emp_id: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
     emp_name: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true,
     },
-    ratting: { 
-        type: DataTypes.FLOAT,
-        allowNull: false
+    ratting: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
     comment: {
-        type: DataTypes.TEXT,
-        allowNull: true
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
-}, {
+  },
+  {
     sequelize,
-    modelName: 'Ratting',
-    tableName: 'rattings', 
+    modelName: "Ratting",
+    tableName: "rattings",
     timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-});
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
 
 module.exports = Ratting;
