0039397883473736b7817ab7598592a443f8863a
Hommlie <sakib@hommlie.com>
2025-07-23 18:06:35 +0530

    update

--- client-connected/src/components/Header/index.jsx
+++ client-connected/src/components/Header/index.jsx
@@ -553,7 +553,7 @@ const offers = [
               </div>
 
               {/* Search Bar */}
-              <div className="hidden lg:block w-[630px] sm:w-full md:w-[500px] lg:w-[630px] mx-auto">
+              <div className="hidden lg:block w-[630px] sm:w-full md:w-[400px] lg:w-[630px] mx-auto">
                 <div className="relative">
                   <input
                     ref={searchInputRef}
