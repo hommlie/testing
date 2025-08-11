c99013ba925c86e8f57b05897a3a9706250b255e
Hommlie <sakib@hommlie.com>
2025-08-11 14:00:35 +0530

    updated

--- client-connected/src/config/config.js
+++ client-connected/src/config/config.js
@@ -4,10 +4,10 @@ const config = {
     GMAP_KEY: "AIzaSyBmaR3DSseRPUCCvGT0Ru8aK-Jrm39NlTE",
     PLACE_ID: "ChIJgxuWZf0_rjsRbJaur3y9Lxo",
     VITE_BASE_URL: "",
-    RAZORPAY_KEY_ID: "rzp_live_ziQnMov0ucEqGL",
+    RAZORPAY_KEY_ID: "rzp_live_xVw2JXPVKb1EVr",
     RAZORPAY_TEST_KEY_ID: 'rzp_test_0vcGsGh8Ebtgd4',
     RAZORPAY_TEST_KEY_SECRET: 'aXnetTExGvqoFQaQTn6hVugq',
-    RAZORPAY_KEY_SECRET: "CAqffXj972VdF1bpVVLi577z"
+    RAZORPAY_KEY_SECRET: "LWXp1cNcTa5lRQ4ht5bsn9Wv"
 };
 
 export default config;
