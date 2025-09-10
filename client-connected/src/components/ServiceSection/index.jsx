0037f073992568cd1d64f6023ed630fdbe9e688f
sakib <sakib@hommlie.com>
2025-09-10 17:55:33 +0530

    updated

--- client-connected/src/components/ServiceSection/index.jsx
+++ client-connected/src/components/ServiceSection/index.jsx
@@ -434,7 +434,7 @@ const ServiceSection = ({ categories }) => {
         {/* Request a Callback Button (visible on all devices) */}
         <div className="mb-5 -mt-9 sm:-mt-0 flex justify-center">
           <button
-            className="bg-[#92B775] text-white px-6 py-2 rounded-md font-semibold hover:bg-[#52852d] transition"
+            className="bg-[#15803d] text-white px-6 py-2 rounded-md font-semibold hover:bg-[#52852d] transition"
             onClick={() => setIsCallbackOpen(true)}
           >
             Request a Callback
